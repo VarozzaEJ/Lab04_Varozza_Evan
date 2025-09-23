880fcb74561fcb0111828b83824e3926a35ced22
Evan Varozza <VarozzaEJ@gmail.com>
2025-09-23 13:02:29 -0600

    init

---
+++ FormatChecker.java
@@ -0,0 +1,9 @@
+package Lab04_Varozza_Evan;
+
+public class FormatChecker {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
